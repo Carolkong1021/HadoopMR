c59a133783dc25cbe98baddecf06df22075352e1
Carolkong1021 <56281399+Carolkong1021@users.noreply.github.com>
2019-10-07 20:54:59 -0400

    Update test4.sh

--- test4.sh
+++ test4.sh
@@ -1,11 +1,16 @@
+
 bash /mapreduce-test/start.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /lab1/input1/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /lab1/output1/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /lab1/input1/
+echo -e "input the start time\n"
+read start
+echo -e "input the end time\n"
+read end
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal /mapreduce-test/mapreduce-test-data/access.log /lab1/input1/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
 -file ../Carolkong1021/mapper3.py -mapper ../Carolkong1021/mapper3.py \
--file ../Carolkong1021/reducer3.py -reducer ../Carolkong1021/reducer3.py \
+-file ../Carolkong1021/reducer3.py -reducer "../Carolkong1021/reducer3.py $start $end" \
 -input /lab1/input1/* -output /lab1/output1/
 /usr/local/hadoop/bin/hdfs dfs -cat /lab1/output1/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /lab1/input1/
